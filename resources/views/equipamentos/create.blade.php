1b07f7b9ffba5735a4f03940ba9c00b8208f1b41
Diegotpereira <diegoteixeirapereira@hotmail.com>
2021-03-16 15:04:57 -0300

    alteração no layout create.blade retirado os botões.

--- resources/views/equipamentos/create.blade.php
+++ resources/views/equipamentos/create.blade.php
@@ -1,16 +1,10 @@
 @extends('layouts.app')
-
 @section('content')
 
 <div class="container">
     <div class="row">
          <h3>Novo</h3>
          @include('equipamentos._form')
-         <div class="form-group">
-             <input type="submit" name="save_eqp" value="Salvar">
-             <input type="submit" name="Cancel" value="Cancelar">
-         </div>
     </div>
-    </form>
 </div>
 @endsection
