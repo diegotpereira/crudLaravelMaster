de6f94834318dbb4be596545fbfd2613dcd550a3
Diegotpereira <diegoteixeirapereira@hotmail.com>
2021-03-16 10:57:40 -0300

    alteração nos arquivos para correção de sintaxe.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
          <meta http-equiv="X-UA-Compatible" content="IE=edge">
          <meta name="viewport" content="width=device-width, initial-scale=1">
 
-         <title>{{ config('app.name', 'Crud Laravel') }}</title>
+         <title>{{ config('app.name', 'Crud com Laravel') }}</title>
           <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
     </head>
 <body>
@@ -13,7 +13,7 @@
          <nav class="navbar navbar-default navbar-static-top">
              <div class="navbar-header">
                   <a href="http://localhost:8000" class="navbar-brand">
-                  {{ config('app.name', Crud Laravel)}}
+                  {{ config('app.name', 'Crud com Laravel')}}
                   </a>
              </div>
              @yield('content')
